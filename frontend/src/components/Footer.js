994603644a750deeba370590907a88d15a28196c
Arushacked <46495591+Arushacked@users.noreply.github.com>
2021-06-25 23:00:51 +0530

    3 by arush

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                Made by DSA
+            </div>
+         );
+    }
+}
+ 
+export default Footer;
